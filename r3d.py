fc4137b36c3b2b843515162f434d4572c192831f
MuhdRayan10 <muhammedrayansavad1@gmail.com>
2025-06-25 19:34:38 +0300

    mov to mp4 rough code

--- r3d.py
+++ r3d.py
@@ -1,4 +1,5 @@
 import subprocess
+import ffmpeg
 import argparse
 import sys
 import os
@@ -9,10 +10,15 @@ def r3d_to_mov(input_path):
     command = ["redline", "--exportPreset", "A", "-i", input_path, "-w", "201", "-R", "4", 
                "--useRMD", "1", "-c", "1", "-G", "32", "--o", output_path]
 
-    
-
     print("Running R3D -> MOV conversion...")
     subprocess.run(command, check=True)
     print(f"Conversion complete: {output_path}")
 
-r3d_to_mov("/Users/rayan/Downloads/sample.R3D")
+def mov_to_mp4(input_path):
+    output_path = os.path.splitext(input_path)[0] + '.mp4'
+
+    print("Running MOV -> MP4 conversion...")
+    ffmpeg.input(input_path).output(output_path, vcodec='libx264', acodec='aac', preset="fast").run(overwrite_output=True)
+    print(f"Conversion complete: {output_path}")
+
+mov_to_mp4("/Users/rayan/Downloads/sample.mov")
